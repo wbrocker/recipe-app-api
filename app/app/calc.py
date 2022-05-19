77ea8121d8d27254a7db9a2a3db4c32b60b79318
Werner <werner@brocker.co.za>
2022-05-19 15:51:05 +0200

    Added DB Dependencies for Docker?

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+    Calculator functions
+"""
+
+def add(x, y):
+    """ Add x and y and resturn result. """
+    return x + y
+
+def subtract(x, y):
+    """Subtract x and y and return results."""
+    return y - x
