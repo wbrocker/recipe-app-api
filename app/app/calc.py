f6c47a7c9eb8b132673940b93c8b04c235a184cb
Werner <werner@brocker.co.za>
2022-05-24 15:44:44 +0200

    DB Startup Testing Implemented

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,12 @@
     Calculator functions
 """
 
+
 def add(x, y):
     """ Add x and y and resturn result. """
+
     return x + y
+
 
 def subtract(x, y):
     """Subtract x and y and return results."""
